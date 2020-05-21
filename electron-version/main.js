12147066992d145f3437f591efbdd2d7f2fbf87e
reesealanj <reesealanj@gwu.edu>
2020-05-21 15:21:00 -0700

    Added ESLint and Formatted JS Files

--- electron-version/main.js
+++ electron-version/main.js
@@ -1,116 +1,116 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
 
-const {app, Menu, BrowserWindow} = electron;
+const { app, Menu, BrowserWindow } = electron;
 
-let mainWindow; 
+let mainWindow;
 
 // On Ready, run Initialization Logic
-app.on('ready', function() {
-    // Create Window
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'main_window.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+app.on("ready", function () {
+  // Create Window
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "main_window.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 
-    // Create and Insert Menu
-    const menuBar = Menu.buildFromTemplate(mainMenu);
-    Menu.setApplicationMenu(menuBar);
+  // Create and Insert Menu
+  const menuBar = Menu.buildFromTemplate(mainMenu);
+  Menu.setApplicationMenu(menuBar);
 
-    // Quit whole application if window closed
-    mainWindow.on('closed', function() {
-        app.quit();
-    });
+  // Quit whole application if window closed
+  mainWindow.on("closed", function () {
+    app.quit();
+  });
 });
 // Menu Template
 const mainMenu = [
-    {
-        label: "File",
-        submenu: [
-            {
-                label: 'Save Drawing as PNG',
-                accelerator: 'CmdOrCtrl+S',
-                click() {
-                    saveScreen();
-                }
-            },
-            {
-                label: 'Quit', 
-                accelerator: 'CmdOrCtrl+Q', 
-                click() {
-                    app.quit();
-                }
-            }
-        ]
-    },
-    {
-        label: "Edit",
-        submenu: [
-            {
-                label: 'Clear Canvas',
-                accelerator: 'CmdOrCtrl+N',
-                click() {
-                    clearCanvas();
-                }
-            }
-        ]
-    },
-    {
-        label: "Help",
-        submenu: [
-            {
-                label: "View Web Version",
-                click() {
-                    electron.shell.openExternal("https://www.rjones.dev/js-paint/");
-                }
-            },
-            {
-                label: "Source Code",
-                click() {
-                    electron.shell.openExternal("https://www.github.com/reesealanj/js-paint/");
-                }
-            }, 
-            {
-                label: "Creator",
-                click() {
-                    electron.shell.openExternal("http://www.linkedin.com/in/reesealanj");
-                }
-            }
-        ]
-    }
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Save Drawing as PNG",
+        accelerator: "CmdOrCtrl+S",
+        click() {
+          saveScreen();
+        },
+      },
+      {
+        label: "Quit",
+        accelerator: "CmdOrCtrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+  {
+    label: "Edit",
+    submenu: [
+      {
+        label: "Clear Canvas",
+        accelerator: "CmdOrCtrl+N",
+        click() {
+          clearCanvas();
+        },
+      },
+    ],
+  },
+  {
+    label: "Help",
+    submenu: [
+      {
+        label: "View Web Version",
+        click() {
+          electron.shell.openExternal("https://www.rjones.dev/js-paint/");
+        },
+      },
+      {
+        label: "Source Code",
+        click() {
+          electron.shell.openExternal(
+            "https://www.github.com/reesealanj/js-paint/"
+          );
+        },
+      },
+      {
+        label: "Creator",
+        click() {
+          electron.shell.openExternal("http://www.linkedin.com/in/reesealanj");
+        },
+      },
+    ],
+  },
 ];
 
-if(process.platform == 'darwin') {
-    mainMenu.unshift({
-        label: ""
-    });
+if (process.platform == "darwin") {
+  mainMenu.unshift({
+    label: "",
+  });
 }
 
-if(process.env.NODE_ENV !== 'production') {
-    mainMenu.push({
-        label: "Developer Tools", 
-        submenu: [
-            {
-                label: 'Toggle Dev Tools',
-                accelerator: 'CmdOrCtrl+I',
-                click(item, focusedWindow) {
-                    focusedWindow.toggleDevTools();
-                }
-            }, 
-            {
-                role: 'reload'                
-            }
-        ]
-    });
+if (process.env.NODE_ENV !== "production") {
+  mainMenu.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle Dev Tools",
+        accelerator: "CmdOrCtrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: "reload",
+      },
+    ],
+  });
 }
 
-function saveScreen() {
+function saveScreen() {}
 
-}
-
-function clearCanvas() {
-
-}
+function clearCanvas() {}
